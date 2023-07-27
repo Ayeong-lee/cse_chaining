cfb2ff109248a7cc9a3a859991eee2de75dfd82a
Ayeong-lee <64763550+Ayeong-lee@users.noreply.github.com>
2023-07-27 21:13:33 +0900

    .

--- configs/mongodb-connection.js
+++
@@ -1,6 +0,0 @@
-const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://ehdcks1224:rkskek1234@cluster0.7hsttm7.mongodb.net/board";
-
-module.exports = function (callback) {
-  return MongoClient.connect(uri, callback);
-};
--- configs/mongoose-connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-const uri =
-  "mongodb+srv://ehdcks1224:rkskek1234@cluster0.7hsttm7.mongodb.net/board?retryWrites=true&w=majority";
-
-module.exports = function () {
-  return mongoose.connect(uri, { useNewUrlParser: true });
-};
