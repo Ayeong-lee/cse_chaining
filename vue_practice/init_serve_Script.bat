3ec710d3d89e399a8efeb6078968e9021134fd48
jeong jin kyu <jjk3969@naver.com>
2023-08-04 13:31:44 +0900

    [fix] script

--- vue_practice/init_serve_Script.bat
+++ vue_practice/init_serve_Script.bat
@@ -1,7 +1,7 @@
 :: Vue Project build
-echo "Start building your VUE project"
 echo "--------------------------------"
 echo "Running Spring boot"
+echo "--------------------------------"
 start /d "../vue-backend/build/libs" java -jar vue-backend-0.0.1-SNAPSHOT.jar
 echo "Running Vue Sever"
 npm run serve
