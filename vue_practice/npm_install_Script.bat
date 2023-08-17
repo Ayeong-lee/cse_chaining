4050908fb672a0bbfd33d81070cfde0cd6eaebde
jeong jin kyu <jjk3969@naver.com>
2023-08-17 13:21:07 +0900

    [add] npm vuex

--- vue_practice/npm_install_Script.bat
+++ vue_practice/npm_install_Script.bat
@@ -4,3 +4,4 @@ START /B /WAIT cmd /c npm install eslint --save-dev
 START /B /WAIT cmd /c npm install --save-dev eslint eslint-plugin-vue
 START /B /WAIT cmd /c npm install --save vue-router
 START /B /WAIT cmd /c npm install jquery --save-dev
+START /B /WAIT cmd /c npm install vuex@next --save
