6a598e6fc0601ad5f053938115387c707fed3aa8
jeong jin kyu <jjk3969@naver.com>
2023-08-17 13:22:56 +0900

    [add&change] login add & board|router change

--- vue_practice/src/main.js
+++ vue_practice/src/main.js
@@ -1,11 +1,12 @@
 import './assets/common.css'
+import store from './store';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-const app = createApp(App).use(router)
+const app = createApp(App).use(router).use(store)
 app.config.globalProperties.$axios = axios;                 //전역변수로 설정 컴포넌트에서 this.$axios 호출할 수 있음
 app.config.globalProperties.$serverUrl = '//localhost:8081' //api server
 app.mount('#app')
