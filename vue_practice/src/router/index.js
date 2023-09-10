0be3a97d9ffe68e1e535a21c4d3f0a09f85c2e7c
jeong jin kyu <jjk3969@naver.com>
2023-09-10 13:21:36 +0900

    [refactoring] erase comment

--- vue_practice/src/router/index.js
+++ vue_practice/src/router/index.js
@@ -1,8 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import PageHome from '@/views/PageHome.vue'
-// import BoardList from '@/views/board/BoardList.vue'
-// import BoardDetail from '@/views/board/BoardDetail.vue'
-// import BoardWrite from '@/views/board/BoardWrite.vue'
 import Board_Rest from "@/views/board/Board_Rest.vue"
 import Login_Rest from '@/views/Login_rest.vue'
 import Account from '@/views/account.vue'
